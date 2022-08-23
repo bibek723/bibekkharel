23bacdeb89699c68172895407d016cc2eb1098ec
kharelbibek723 <kharelbibek723@gmail.com>
2022-08-23 21:38:25 +0545

    Everything is updated

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.urls import reverse
-from tinymce import HTMLField
+from tinymce.models import HTMLField
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
@@ -17,7 +17,7 @@ class Blog(models.Model):
     title = models.CharField(max_length=50)
     thumbnail = models.ImageField(upload_to='blog/images/', blank=True)
     comment_count = models.IntegerField(default=0)
-    content = HTMLField()
+    content = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
     publisher = models.ForeignKey(Author, on_delete=models.CASCADE)
 
